aa79f965772ecfff72a16f397bead5aa44bb7cfb
ksoon1985 <ksoon1985@naver.com>
2022-02-03 20:39:01 +0900

    초기 셋팅

---
+++ src/main/java/kr/co/sol/SpringbootShopApplication.java
@@ -0,0 +1,13 @@
+package kr.co.sol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/sol/SpringbootShopApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.sol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
